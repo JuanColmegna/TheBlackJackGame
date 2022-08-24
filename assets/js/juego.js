d97abd64cde7fd5a11bc5eafdfa5538f4a5765de
JuanColmegna <juanfmcolmegna@gmail.com>
2022-08-24 19:33:54 -0300

    primer version de juego de blackjack

---
+++ assets/js/juego.js
@@ -0,0 +1,147 @@
+/*
+* 2C = tow of clubs (dos de treboles)
+* 2D = tow of diamonds (dos de diamantes)
+* 2H = tow of hearts (dos de corazones)
+* 2s = tow of spades (dos de espadas)
+*/
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K']
+
+let puntosJugador = 0;
+let puntosComputadora = 0;
+
+// referencias en HTML
+
+const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+const btnNuevo = document.querySelector('#btnNuevo');
+
+
+const puntosHTML = document.querySelectorAll('small');
+
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
+
+// esta funcion crea una nueva baraja
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for(let tipo of tipos){
+            deck.push(i + tipo);
+        }
+    }
+    for (let tipo of tipos){
+        for(let esp of especiales){
+            deck.push(esp + tipo)
+        }
+    }
+    deck = _.shuffle(deck);
+    console.log(deck);
+    return deck;
+}
+
+crearDeck();
+
+// esta funcion me permite tomar una carta
+const pedirCarta = () =>{
+    if(deck.length === 0){
+        throw 'No hay cartas en la baraja';
+    }
+    const carta = deck.pop();
+    return carta;
+}
+
+// esta funcion determina el valor de la carta
+const valorCarta = (carta) =>{
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) 
+        ? (valor === 'A') ? 11 : 10
+        : (valor * 1);
+}
+
+// turno de la computadora
+
+const turnoComputadora = (puntosMinimos) => {
+    do {
+    const carta = pedirCarta();
+    puntosComputadora = puntosComputadora + valorCarta(carta);
+    puntosHTML[1].innerText = puntosComputadora;
+
+    // <img class="carta" src="./assets/cartas/.png" alt="carta"> 
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `./assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasComputadora.append(imgCarta);
+
+    if (puntosMinimos > 21){
+        break;
+    }
+
+    } while ( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) );
+
+
+    setTimeout(()=> {
+        if (puntosComputadora === puntosMinimos){
+            alert('Nadie gana');
+        } else if (puntosMinimos > 21){
+            alert('Computadora gana');
+        } else if ( puntosComputadora > 21){
+            alert('Jugador gana');
+        } else {
+            alert('Computadora gana');
+        }
+    }, 10);
+}
+
+// eventos
+btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+    puntosJugador = puntosJugador + valorCarta(carta);
+    puntosHTML[0].innerText = puntosJugador;
+
+    // <img class="carta" src="./assets/cartas/.png" alt="carta"> 
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `./assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
+
+    if(puntosJugador > 21){
+        console.warn('lo siento mucho ya perdiste');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora( puntosJugador );
+    } else if (puntosJugador === 21){
+        console.warn('21, genial');
+        btnDetener.disabled = true;
+        btnPedir.disabled = true;
+        turnoComputadora( puntosJugador );
+    } 
+})
+
+
+
+btnDetener.addEventListener('click', () => {
+    btnDetener.disabled = true;
+    btnPedir.disabled = true;
+    turnoComputadora(puntosJugador);
+})
+
+
+btnNuevo.addEventListener('click', () => {
+    console.clear();
+    deck = [];
+    deck = crearDeck();
+
+    puntosJugador = 0;
+    puntosComputadora = 0;
+
+    puntosHTML[0].innerText = 0;
+    puntosHTML[1].innerText = 0;
+
+    divCartasComputadora.innerHTML = '';
+    divCartasJugador.innerHTML = '';
+
+    btnDetener.disabled = false;
+    btnPedir.disabled = false;
+})
